f92e7432fd137f4d3dd8db5c4dd0cec4b5fe13d0
srujanaa6 <srujanaparesh@gmail.com>
2025-09-01 19:09:49 +0530

    modified readme

--- src/utils/hide_keyboard.py
+++ src/utils/hide_keyboard.py
@@ -2,7 +2,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 from .logger import message_logger
 
+
+
 def hide_keyboard(driver):
+    """
+    Hides the on-screen keyboard on LambdaTest mobile devices 
+    to prevent it from blocking other elements during test execution.
+    """
     try:
         driver.execute_script("document.activeElement.blur();")
         time.sleep(0.5) 
